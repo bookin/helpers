132bc70db3c1ecc25416f656cd580255495c8030
Anton <toh1napocht@yandex.ru>
2015-10-08 17:42:07 +0300

    helpers init

---
+++ yii2/extensions.php
@@ -0,0 +1,65 @@
+<?php
+namespace bookin\helpers\yii2;
+
+use yii\helpers\ArrayHelper;
+use \Yii;
+
+/**
+ * Class Extensions
+ */
+class Extensions{
+
+    /**
+     * @param array $extensions
+     * <code>
+     * 'phpoffice/phpword'=>[
+     *      'name'=>'phpoffice/phpword',
+     *      'alias'=>
+     *          '&#64;phpoffice/phpword'=>'&#64;extensions/phpoffice/phpword',
+     *       ]
+     * ];
+     * </code>
+     * @param null $extensionsDir Path to extensions dir Default - @app/extensions
+     * @param null $vendorExtension Path to vendor extensions list Default - dirname(__DIR__). "/vendor/yiisoft/extensions.php"
+     * @return array
+     */
+    public static function add($extensions=[], $extensionsDir=null, $vendorExtension=null){
+        if(empty($extensionsDir))
+            $extensionsDir =  dirname(__DIR__). DIRECTORY_SEPARATOR . 'extensions';
+
+        if(empty($vendorExtension))
+            $vendorExtension = dirname(__DIR__). "/vendor/yiisoft/extensions.php";
+
+        $vendorExtension = require_once $vendorExtension;
+
+        Yii::setAlias('@extensions', $extensionsDir);
+
+        foreach ($extensions as $extension) {
+            if (isset($extension['alias'])) {
+                foreach ($extension['alias'] as $alias => $path) {
+                    Yii::setAlias($alias, $path);
+                }
+            }
+        }
+
+        $merge = ArrayHelper::merge(
+            $extensions,
+            $vendorExtension
+        );
+
+        return $merge;
+    }
+
+    protected function __construct(){
+
+    }
+
+    private function __clone()
+    {
+    }
+
+
+    private function __wakeup()
+    {
+    }
+}
